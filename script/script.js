064db49546b020d80f76fb1741d17f542307359f
DESKTOP-G02PH9I\AHMED EID <ayar8751@gmail.com>
2023-01-17 21:39:45 +0200

    make scroll button

--- script/script.js
+++ script/script.js
@@ -2,11 +2,31 @@ console.log("hello");
 const menu = document.querySelector('.menu-nav');
 const nav = document.querySelector('.nav');
 const allLinks = document.querySelectorAll("a");
+const header = document.querySelector(".header");
+const navContainer = document.querySelector(".nav-container");
+const goTopButton = document.querySelector(".go-to-up");
+
+const navAbserver = new IntersectionObserver(function(watchEntry, navObserver){
+    console.log(watchEntry[0].isIntersecting);
+    if(!watchEntry[0].isIntersecting){
+        navContainer.classList.add('nav-scroll');
+        // goTopButton.classList.add('nav-scroll');
+    } else {
+        navContainer.classList.remove('nav-scroll');
+        // goTopButton.classList.remove('nav-scroll');
+    } 
+},{
+    rootMargin:'-50px',
+    threshold:0
+});
+navAbserver.observe(header);
+
 if(menu){
     menu.addEventListener('click', function (){
         nav.classList.toggle('is-active');
     })
 }
+
 allLinks.forEach( function (link){
     if(link.classList.contains("nav-link")){
         link.addEventListener('click', function (){
@@ -15,3 +35,4 @@ allLinks.forEach( function (link){
     }
     
 })
+
