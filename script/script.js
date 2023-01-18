533b608fed25b9496c0505416780863c7d2fd190
DESKTOP-G02PH9I\AHMED EID <ayar8751@gmail.com>
2023-01-18 13:16:30 +0200

    fixed header

--- script/script.js
+++ script/script.js
@@ -1,18 +1,16 @@
-console.log("hello");
 const menu = document.querySelector('.menu-nav');
 const nav = document.querySelector('.nav');
 const allLinks = document.querySelectorAll("a:link");
 const header = document.querySelector(".header");
 const navContainer = document.querySelector(".nav-container");
-const goTopButton = document.querySelector(".go-to-up");
+
+// handling change navContainer by scrolling down
 const navAbserver = new IntersectionObserver(function(watchEntry, navObserver){
     console.log(watchEntry[0].isIntersecting);
     if(!watchEntry[0].isIntersecting){
         navContainer.classList.add('nav-scroll');
-        // goTopButton.classList.add('nav-scroll');
     } else {
         navContainer.classList.remove('nav-scroll');
-        // goTopButton.classList.remove('nav-scroll');
     } 
 },{
     rootMargin:'-50px',
@@ -20,11 +18,12 @@ const navAbserver = new IntersectionObserver(function(watchEntry, navObserver){
 });
 navAbserver.observe(header);
 
-
+// handling open and close nav in mobile
 menu.addEventListener('click', function (){
     nav.classList.toggle('is-active');
 })
 
+// handling close nav after pressing any link
 allLinks.forEach( function (link){
     if(link.classList.contains("nav-link")){
         link.addEventListener('click', function (){
